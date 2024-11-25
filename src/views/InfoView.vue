7589fa45f6b8338f5551fe9cfa28f1fa7917fbd4
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2024-11-25 12:46:06 +0700

    feat: add Info Page

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    INFO
+    <p>
+      © 2024 Bunnasorn Kaewsiri, All rights reserved.
+    </p>
+    <hr>
+    <p>
+      ข้อความและรูปภาพที่เกี่ยวข้องกับ Pokémon Trading Card Game รวมไปถึงรูปภาพการ์ด เป็นลิขสิทธิ์ของ The Pokémon
+      Company Internaltional, inc <br>
+      เว็บไซต์ THDex เป็นเว็บไซต์ที่ถูกจัดทำโดยบุคคลที่ 3 ไม่ได้จัดทำ ได้รับการรับรอง สนับสนุน หรือมีความเกี่ยวข้องกับ
+      Nintendo, Game Freak, Niantic, Wizards
+      of the Coast หรือ The Pokémon Company แต่อย่างใด
+    </p>
   </div>
 </template>
 
@@ -8,4 +18,15 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+div {
+  text-align: center;
+  color: var(--gray);
+}
+
+hr {
+  margin: 1em;
+  width: 90%;
+  border: 1px solid var(--dark);
+}
+</style>
