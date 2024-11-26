cf4880f29c846d999141df9a8b95535a6884c4e1
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2024-11-26 08:09:22 +0700

    perf: Add Skeleton for lazy load

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    <img loading="lazy" :src="'https://asia.pokemon-card.com/th/card-img/' + props.cardID + '.png'" alt="props.cardID">
-  </div>
+  <img v-show="isLoaded" :src="'https://asia.pokemon-card.com/th/card-img/' + props.cardID + '.png'" :alt="props.cardID"
+    @load="onImageLoaded">
+  <Skeleton v-if="!isLoaded" containerClass="skeleton" childClass="skeleton-item" />
+
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const isLoaded = ref(false)
 
 const props = defineProps({
   cardID: String
 })
+
+const onImageLoaded = () => {
+  isLoaded.value = true
+}
 
 </script>
 
